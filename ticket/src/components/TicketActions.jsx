49c853a99e38b3644e42d3578d791c7a8db8ad04
bhig-sheydi <bhigsheydi@gmail.com>
2025-02-14 08:22:22 -0800

    delayed by back end

--- ticket/src/components/TicketActions.jsx
+++ ticket/src/components/TicketActions.jsx
@@ -1,6 +1,6 @@
 const TicketActions = ({ barcode }) => {
     return (
-      <div className="mt-4 space-y-2 w-full max-w-md flex justify-center h-full items-center">
+      <div className=" space-y-2 w-full max-w-md flex justify-center h-full items-center">
         <div className="w-[80%] bg-white h-10 flex items-center justify-center text-black font-bold tracking-wide text-base px-4 rounded-lg shadow-md">
           {barcode}
         </div>
