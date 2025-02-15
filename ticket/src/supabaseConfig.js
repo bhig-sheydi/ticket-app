866a371d3067f7839eb59b1c742d967fc14fb546
bhig-sheydi <bhigsheydi@gmail.com>
2025-02-15 06:11:02 -0800

    delayed by back end

--- ticket/src/supabaseConfig.js
+++ ticket/src/supabaseConfig.js
@@ -1,35 +1,28 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = "https://sfpgcjkmpqijniyzykau.supabase.co";
-const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNmcGdjamttcHFpam5peXp5a2F1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mjc5ODY0ODgsImV4cCI6MjA0MzU2MjQ4OH0.WiT1sOtfobReGst9Rf56EsqXNziMUWigLUG6VxhKQZs"
-console.log('Supabase Anon Key:', supabaseAnonKey); 
-
-
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
-
 
 const fetchAuthenticatedUser = async () => {
     const { data: { user }, error } = await supabase.auth.getUser();
   
     if (error) {
-      console.error('Error fetching user:', error);
-      return null;
+        console.error('Error fetching user:', error);
+        return null;
     }
     
     return user;
-  };
-  
-  
-  const logUserName = async () => {
-    const user = await fetchAuthenticatedUser();
-  
-    if (user) {
-      console.log(user.user_metadata?.user_name || 'Username not found');
-      console.log(user.email || 'Email not found');
-    }
-  };
-  
-  
-  logUserName();
+};
 
+const logUserName = async () => {
+    const user = await fetchAuthenticatedUser();
+
+    if (user) {
+        console.log(user.user_metadata?.user_name || 'Username not found');
+        console.log(user.email || 'Email not found');
+    }
+};
+
+logUserName();
